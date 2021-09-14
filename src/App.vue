bc7f218ea0bb0e4f37a2588756ab7190062a5e66
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-09-14 23:58:37 +0300

    App.vue updated for Google clone

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header></Header>
+  <Main></Main>
+  <Footer></Footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  height: 100%;
+}
+html {
+  font-size: 65%;
+}
+body {
+  display: grid;
+  grid-template-rows: auto 3fr 1fr;
+  font-family: Arial, sans-serif;
+  font-size: 1.6rem;
+}
+a {
+  text-decoration: none;
+  padding: 0 3px;
+}
+ul {
+  list-style-type: none;
 }
 </style>
