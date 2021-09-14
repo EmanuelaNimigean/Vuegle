14876f7b65c9e761216243a92b0264454e4cc8a0
EmanuelaNmg <emanuela.nimigean@principal33.com>
2021-09-15 00:17:53 +0300

    split into components

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<header class="header">
+        <nav class="navBar">
+            <ul class="navLinks">
+                <li><a href="#" class="navLink">Gmail</a></li>
+                <li><a href="#" class="navLink">Imagini</a></li>
+            </ul>
+            <div class="aplicatii">
+                <img src="../assets/aplicatii.png" />
+            </div>
+            <div class="cont">
+                <img src="../assets/cont.png" style="border-radius: 50%;" />
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+@import '/src/styles/header.css';
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+<main class="main">
+        <img src="/src/assets/gLogo.png" class="logo" />
+
+        <div class="searchBar">
+            <img src="src/assets/search.png" class="searchIcon" />
+            <input type="search" class="searchInput" />
+            <img src="/src/assets/keyboard.png" class="keyboardIcon" />
+            <img src="/src/assets/mic.png" class="micIcon" />
+        </div>
+
+        <div class="buttons">
+            <button class="button">Căutare Google</button>
+            <button class="button">Mă simt norocos</button>
+        </div>
+
+        <div class="languages">
+            <p>Google oferit în: </p>
+            <div class="language">
+                <a class="lang" href="">English</a>
+                <a class="lang" href="">magyar</a>
+                <a class="lang" href="">Deutsch</a>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+@import '/src/styles/main.css';
+</style>
